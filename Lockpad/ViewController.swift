5e7be80b6eb2fa998a2bd696cce18c99b2c70435
brady cusack <brady.cusack@clearcodex.com>
2017-06-12 16:28:13 -0400

    Initial Commit

---
+++ Lockpad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lockpad
+//
+//  Created by Brady Cusack on 6/12/17.
+//  Copyright © 2017 Brady Cusack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
