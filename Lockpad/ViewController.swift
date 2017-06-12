2b5856fd7ab043c74601e59b21aeb02f8e7b7e2a
brady cusack <brady.cusack@clearcodex.com>
2017-06-12 17:12:58 -0400

    Created login screen with Key picture, takes a four digit numerical code to login, the authentication is not setup with a server yet though. Started to set up app to have a table view listing all of the text documents that have yet to be made!

--- Lockpad/ViewController.swift
+++ Lockpad/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var keyImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var passcodeTF: UITextField!
+    @IBOutlet weak var unlockBttn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onUnlockClick(_ sender: UIButton) {
+        
+        if(passcodeTF.text == "1234"){
+            performSegue(withIdentifier: "AuthenticationSuccess", sender: self)
+        } else {
+            let failedLoginAlert = UIAlertController(title: "Login Failed", message: "Please enter the correct passcode!", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+            failedLoginAlert.addAction(defaultAction)
+            present(failedLoginAlert, animated: true, completion: nil)
+        }
+        
+    }
 
 }
 
---
+++ PadTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PadTableViewController.swift
+//  Lockpad
+//
+//  Created by Brady Cusack on 6/12/17.
+//  Copyright © 2017 Brady Cusack. All rights reserved.
+//
+
+import UIKit
+
+class PadTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+
+}
